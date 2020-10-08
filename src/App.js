dd18d68505964d451248ca6014e7487fbd70c7b7
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-08 13:45:26 +0530

    add Label To toggle DarkMode

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import styles from "./App.module.css";
 import coronaImage from "./images/image.png";
 import { fetchData } from "./api";
 import Switch from '@material-ui/core/Switch';
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 class App extends React.Component {
   state = {
@@ -30,16 +31,33 @@ class App extends React.Component {
   render() {
     const { data, country, darkMode } = this.state;
     return (
-      <div className={`${!darkMode ? styles.container : styles.container__darkMode}`}>
-        <img className={`${!darkMode ? styles.image : styles.image__darkMode}`} src={coronaImage} alt="COVID-19" />
-        <Switch
-          checked={darkMode}
-          onChange={this.toggleMode}
-          name="checkedA"
-          inputProps={{ 'aria-label': 'secondary checkbox' }}
+      <div
+        className={`${
+          !darkMode ? styles.container : styles.container__darkMode
+        }`}
+      >
+        <img
+          className={`${!darkMode ? styles.image : styles.image__darkMode}`}
+          src={coronaImage}
+          alt="COVID-19"
+        />
+        <FormControlLabel
+          control={
+            <Switch
+              checked={darkMode}
+              onChange={this.toggleMode}
+              name="checkedA"
+              inputProps={{ "aria-label": "secondary checkbox" }}
+            />
+          }
+          label="DarkMode"
+          labelPlacement="start"
         />
         <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} darkMode={darkMode} />
+        <CountryPicker
+          handleCountryChange={this.handleCountryChange}
+          darkMode={darkMode}
+        />
         <Chart data={data} country={country} />
       </div>
     );
